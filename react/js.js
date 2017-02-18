67d3b8d238260a78454187c29ecb52866e3f80cb
yuna <aquarionyusan@gmail.com>
2017-02-18 11:43:23 +0900

    practice react

---
+++ react/js.js
@@ -0,0 +1,49 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//ヘッダの定義
+var Header = React.createClass({
+    render: function(){
+        return (
+            <h1>ヘッダです</h1>
+        );
+    }
+});
+
+//ボディの定義
+var Body = React.createClass({
+    render: function(){
+        return (
+            <h1>ボディです</h1>
+        );
+    }
+});
+
+//フッタの定義
+var Footer = React.createClass({
+    render: function(){
+        return (
+            <h1>フッタです</h1>
+        );
+    }
+});
+
+//コンポーネントを一つにまとめる
+var Index = React.createClass({
+    render:function(){
+        return (
+            <div>
+            <Header/>
+            <hr/>
+            <Body/>
+            <hr/>
+            <Footer/>
+            </div>
+        );
+    }
+});
+
+React.render(
+<Index />,
+    document.getElementById('content')
+);
